81d04a84d1b9be581258f38c814f95a5860bdd0e
Ayush <ayush@parijat.asia>
2023-07-13 00:36:01 +0545

    sp view zen x

--- app.js
+++ app.js
@@ -3,3 +3,15 @@
 document.querySelector(".header__feature-breadcrumb").addEventListener("click",()=>{
   console.log("work")
 })
+
+if(window.innerWidth < 768){
+  window.addEventListener("scroll" , () => {
+    if (scrollY > 600){
+      document.querySelector(".header__wrapper").style.visibility = "visible";
+      document.querySelector(".header").style.background = "#d9d9d9";
+    }else{
+      document.querySelector(".header__wrapper").style.visibility = "hidden"
+      document.querySelector(".header").style.background = "transparent";
+    }
+  })
+}
